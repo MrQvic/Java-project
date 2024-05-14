e6183b8faf75a5ed60b6e21d4bf8edfa90ac4612
Adam Mrkva <github@mrkvic.com>
2024-05-14 23:47:36 +0200

    added package into maven

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,6 @@ module org.openjfx.javaproject {
     requires javafx.fxml;
     requires org.json;
 
-
     opens org.openjfx.javaproject to javafx.fxml;
     exports org.openjfx.javaproject;
-    exports org.openjfx.javaproject.room;
-    opens org.openjfx.javaproject.room to javafx.fxml;
-    exports org.openjfx.javaproject.common;
-    opens org.openjfx.javaproject.common to javafx.fxml;
 }
---
+++ src/main/java/org/openjfx/javaproject/Main.java
@@ -0,0 +1,7 @@
+package org.openjfx.javaproject;
+
+public class Main {
+    public static void main(String[] args) {
+        RobotSimulator.main(args);
+    }
+}
